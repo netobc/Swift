43b866311d98f679fa41d59afd73cd9616ca2fdd
Ernesto De La Trinidad Vidana <neto@Ernestos-MacBook-Pro.local>
2016-01-03 23:48:11 -0800

    added vars

---
+++ gitfun.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var neto = 15
+
+var net2 = 5
+
+var sum = neto + net2
+
+var mult = neto * net2
+
+var rest = neto - net2
+
+var div = neto / net2
+
+var st = 1, nd = 2, rd = 3
+
+let uno = 4, dos = 5, tres = 6
+
+var punshis: Double
+
+var panza: Int = 423
+
+var queonda = "Hello"
+
+var supi = "World"
+
+var saaa = queonda + supi
+
+saaa = queonda+" \(supi)"
